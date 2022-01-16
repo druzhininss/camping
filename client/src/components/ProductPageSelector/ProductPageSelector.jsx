09deb631c0de60e96b4eb5c23c021ba64193d8ac
SemyonSergeev <semyonsergeev30@gmail.com>
2022-01-16 18:36:19 +0300

    Хотел сделать изменение цвета кнопки через состояние, но не сумел решить вопрос, позже попробую сделать иначе

--- client/src/components/ProductPageSelector/ProductPageSelector.jsx
+++ client/src/components/ProductPageSelector/ProductPageSelector.jsx
@@ -1,16 +1,43 @@
 import React from "react";
+import { useState } from "react";
 
 const ProductPageSelector = () => {
+  const classes = ["list-group-item active"];
+  console.log(classes);
+
+  const [isActive, isSetActive] = useState(false);
+
+  const handleClick = () => {
+    classes.push(" active");
+    console.log(classes);
+
+    isSetActive(true);
+  };
+
   return (
-    <div>
-      <ul className="list-group d-flex flex-row">
-        <li className="list-group-item">Фото</li>
-        <li className="list-group-item">Описание</li>
-        <li className="list-group-item">Преимущества</li>
+    <ul className="list-group d-flex flex-row">
+      {isActive ? (
+        <li
+          className={classes.join(" ")}
+          aria-current="true"
+          onClick={handleClick}
+        >
+          Фото
+        </li>
+      ) : (
+        <li className={classes} onClick={handleClick}>
+          Фото
+        </li>
+      )}
+
+      {/* <li className={classes} onClick={handleClick}>
+          Описание
+        </li> */}
+
+      {/* <li className="list-group-item">Преимущества</li>
         <li className="list-group-item">Схема</li>
-        <li className="list-group-item">Отзывы</li>
-      </ul>
-    </div>
+        <li className="list-group-item">Отзывы</li> */}
+    </ul>
   );
 };
 
