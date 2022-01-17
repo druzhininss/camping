0a7f1424905b167223ae3de0b4b279a96bd6f7d5
SemyonSergeev <semyonsergeev30@gmail.com>
2022-01-17 13:36:17 +0300

    Сделал рабочий вариант слайдера, пока без кнопок

--- client/src/components/ProductPageSlider/ProductPageSlider.jsx
+++ client/src/components/ProductPageSlider/ProductPageSlider.jsx
@@ -1,44 +1,32 @@
 import React from "react";
+import Slider from "react-slick";
 import slide1 from "../../assets/DSCF2406.jpg";
 import slide2 from "../../assets/DSCF2407w.jpg";
 import slide3 from "../../assets/DSCF2415.jpg";
 import slide4 from "../../assets/DSCF2417w.jpg";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
 const listPhoto = [slide1, slide2, slide3, slide4];
 
 const ProductPageSlider = () => {
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrows: true,
+  };
   return (
-    <div
-      id="carouselExampleControls"
-      className="carousel carousel-dark slide col-md-5"
-      data-bs-ride="carousel"
-      data-bs-interval="false"
-    >
-      <div className="carousel-inner">
-        {listPhoto.map((photo) => (
-          <div className="carousel-item active" key={photo}>
-            <img src={photo} className="d-block w-100" alt="..." />
+    <div style={{ maxWidth: 1260, margin: "auto" }}>
+      <Slider {...settings}>
+        {listPhoto.map((slide, index) => (
+          <div key={index}>
+            <img src={slide} alt='true'/>
           </div>
         ))}
-      </div>
-      <button
-        className="carousel-control-prev"
-        type="button"
-        data-bs-target="#carouselExampleControls"
-        data-bs-slide="prev"
-      >
-        <span className="carousel-control-prev-icon" aria-hidden="true"></span>
-        <span className="visually-hidden">Previous</span>
-      </button>
-      <button
-        className="carousel-control-next"
-        type="button"
-        data-bs-target="#carouselExampleControls"
-        data-bs-slide="next"
-      >
-        <span className="carousel-control-next-icon" aria-hidden="true"></span>
-        <span className="visually-hidden">Next</span>
-      </button>
+      </Slider>
     </div>
   );
 };
