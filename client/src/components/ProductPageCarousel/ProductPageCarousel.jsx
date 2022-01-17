3e1f0793c1e209f715f1ac9a61a0720dc368af18
SemyonSergeev <semyonsergeev30@gmail.com>
2022-01-17 15:48:54 +0300

    Карусель под слайдером, пока не связаны между собой

--- client/src/components/ProductPageCarousel/ProductPageCarousel.jsx
+++ client/src/components/ProductPageCarousel/ProductPageCarousel.jsx
@@ -1,32 +1,30 @@
 import React from "react";
+import Slider from "react-slick";
 import slide1 from "../../assets/DSCF2406.jpg";
 import slide2 from "../../assets/DSCF2407w.jpg";
 import slide3 from "../../assets/DSCF2415.jpg";
 import slide4 from "../../assets/DSCF2417w.jpg";
 
+const listPhoto = [slide1, slide2, slide3, slide4];
+
 const ProductPageCarousel = () => {
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 3,
+    slidesToScroll: 3,
+  };
   return (
-    <div
-      id="carouselExampleControls"
-      className="carousel slide"
-      data-bs-ride="carousel"
-      data-bs-interval={null}
-    >
-      <div className="carousel-inner">
-        <div className="carousel-item active">
-          <img src={slide1} className="d-block w-10" alt="..." />
-        </div>
-        <div className="carousel-item">
-          <img src={slide2} className="d-block w-10" alt="..." />
-        </div>
-        <div className="carousel-item">
-          <img src={slide3} className="d-block w-10" alt="..." />
-        </div>
-        <div className="carousel-item">
-          <img src={slide4} className="d-block w-10" alt="..." />
-        </div>
-      </div>
-    </div> 
+    <div style={{ maxWidth: 700, margin: "auto" }}>
+      <Slider {...settings}>
+        {listPhoto.map((photo, index) => (
+          <div key={index}>
+            <img src={photo} alt="true" style={{width: 128, height: 86}}/>
+          </div>
+        ))}
+      </Slider>
+    </div>
   );
 };
 
