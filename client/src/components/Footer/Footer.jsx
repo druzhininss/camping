486054f1e0c0a193e41e3603aeb1b6c0b1983d74
Denis <bokudenis065@gmail.com>
2022-01-18 12:25:17 +0300

    footer done

--- client/src/components/Footer/Footer.jsx
+++ client/src/components/Footer/Footer.jsx
@@ -1,5 +1,9 @@
 import React from 'react';
 import style from './Footer.module.css';
+import Facebook from '../../assets/Facebook.svg';
+import Instagram from '../../assets/Instagram (filled).svg';
+import Youtube from '../../assets/YouTube.svg';
+import Logo from '../../assets/logo.jpg';
 
 function Footer() {
   return (
@@ -7,25 +11,27 @@ function Footer() {
         <div className={style.container}>
           <div className={style.footer}>
             <div className={style.foot}>
-                <p className={style.text}>We offer comfortable spaces, cozy cafe, high-speed internet, spacious parking area and many more for your best workspaces and meetings</p>
+                <img className={style.logo} src={Logo} alt='logo' />
+            </div>
+               <div className={style.item}>
                 <ul className={style.social}>
-                  <li className={style.item}> <a className={style.link} href=""><img src="/images/Instagram (filled).svg" alt="inst" /></a> </li>
-                  <li className={style.item}> <a className={style.link} href=""><img src="/images/YouTube.svg" alt="youtube" /></a> </li>
-                  <li className={style.item}> <a className={style.link} href=""><img src="/images/Facebook.svg" alt="face" /></a> </li>
-                </ul>
-               </div>
-              <div className={style.item}>
-                <ul className={style.social}>
-                  <li className={style.title}>Company</li>
-                  <li className={style.list}>  <a className={style.link} href="">About Us</a> </li>
-                  <li className={style.list}>  <a className={style.link} href="">Pricing</a> </li>
-                  <li className={style.list}>  <a className={style.link} href="">Careers</a> </li>
-                  <li className={style.list}>  <a className={style.link} href="">Press</a> </li>
+                  <li className={style.title}>ПОДПИСЫВАЙТЕСЬ НА НАС</li>
+                  <div className={style.socialPic}>
+                  <li className={style.item}> <a className={style.link} href=""><img src={Instagram} alt="inst" /></a> </li>
+                  <li className={style.item}> <a className={style.link} href=""><img src={Youtube} alt="youtube" /></a> </li>
+                  <li className={style.item}> <a className={style.link} href=""><img src={Facebook} alt="face" /></a> </li>
+                  </div>
                 </ul>
               </div>
               <div className={style.item}>
                 <ul className={style.social}>
-                  <li className={style.title}>Contact Us</li>
+                  <li className={style.title}>АДРЕС</li>
+                  <li className={style.list}>  <p className={style.link} href="">Кирочная 19, строение 2,<br/> Санкт-Петербург, Россия</p> </li>
+                </ul>
+              </div>
+              <div className={style.item}>
+                <ul className={style.social}>
+                  <li className={style.title}>КОНТАКТЫ</li>
                   <li className={style.item}> www.camping.com</li>
                   <li className={style.item}>P.  (+8800) 4670000</li>
                 </ul>
