7355c2380120806d9986e6363d3d077687b48eed
SemyonSergeev <semyonsergeev30@gmail.com>
2022-01-18 16:39:27 +0300

    Сделал вывод описания из store

--- client/src/components/ProductPage/ProductPage.jsx
+++ client/src/components/ProductPage/ProductPage.jsx
@@ -1,4 +1,6 @@
 import React from "react";
+import { useSelector } from "react-redux";
+import { useParams } from "react-router-dom";
 import ProductPageCarousel from "../ProductPageCarousel/ProductPageCarousel";
 import ProductPageHeader from "../ProductPageHeader/ProductPageHeader";
 import ProductPageInfo from "../ProductPageInfo/ProductPageInfo";
@@ -6,6 +8,11 @@ import ProductPageSelector from "../ProductPageSelector/ProductPageSelector";
 import ProductPageSlider from "../ProductPageSlider/ProductPageSlider";
 
 const ProductPage = () => {
+  const { listProductsAll } = useSelector((state) => state.productsReducers); // TODO: change reducer name
+  const { productId } = useParams();
+  const product = listProductsAll?.find(
+    (product) => product.product_id === +productId
+  );
   return (
     <div>
       <div>
@@ -22,24 +29,8 @@ const ProductPage = () => {
         <ProductPageInfo />
       </div>
       <div>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis
-          fuga eos voluptatum soluta, similique et perferendis hic adipisci
-          deserunt enim iure dignissimos placeat eveniet illo nemo temporibus
-          voluptates odit veritatis.
-        </p>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis
-          fuga eos voluptatum soluta, similique et perferendis hic adipisci
-          deserunt enim iure dignissimos placeat eveniet illo nemo temporibus
-          voluptates odit veritatis.
-        </p>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis
-          fuga eos voluptatum soluta, similique et perferendis hic adipisci
-          deserunt enim iure dignissimos placeat eveniet illo nemo temporibus
-          voluptates odit veritatis.
-        </p>
+        <h2>Описание</h2>
+        <div>{product['Описание']}</div>
       </div>
     </div>
   );
