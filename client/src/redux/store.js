eeede1e7bb861e0de18865f268670ce04aea4f46
Denis <bokudenis065@gmail.com>
2022-01-18 17:38:22 +0300

    localstore

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -3,10 +3,35 @@ import { composeWithDevTools } from 'redux-devtools-extension';
 import createSagaMiddleware from 'redux-saga';
 import { rootReducer } from './reducers/rootReducer';
 import { myWatcher } from './sagas/saga';
+import { persistStore, persistReducer } from 'redux-persist'
+import storage from 'redux-persist/lib/storage';
 
+// const sagaMiddleware = createSagaMiddleware();
+
+
+// export const store = createStore(
+//   rootReducer,
+//   composeWithDevTools(applyMiddleware(sagaMiddleware))
+// );
+
+const persistConfig = {
+  key: 'root',
+  storage,
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
 const sagaMiddleware = createSagaMiddleware();
+let store = createStore(persistedReducer, applyMiddleware(sagaMiddleware));
+let persistor = persistStore(store);
+export { store, persistor, sagaMiddleware };
 
+// export default function configureStore() {
+//   const sagaMiddleware = createSagaMiddleware()
+//   return {
+//     ...createStore(rootReducer, applyMiddleware(sagaMiddleware)),
+//     runSaga: sagaMiddleware.run,
+//   }
+// }
 
-export const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(sagaMiddleware)));
 
 sagaMiddleware.run(myWatcher);
