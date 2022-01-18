454e6cc3606055f420d4afcbc84d8bf8899e8345
SemyonSergeev <semyonsergeev30@gmail.com>
2022-01-18 20:48:26 +0300

    Добавил картинки в папку assets

--- client/src/components/ProductPageSlider/ProductPageSlider.jsx
+++ client/src/components/ProductPageSlider/ProductPageSlider.jsx
@@ -1,15 +1,23 @@
 import React from "react";
 import Slider from "react-slick";
-import slide1 from "../../assets/DSCF2406.jpg";
-import slide2 from "../../assets/DSCF2407w.jpg";
-import slide3 from "../../assets/DSCF2415.jpg";
-import slide4 from "../../assets/DSCF2417w.jpg";
+import { useSelector } from "react-redux";
+import { useParams } from "react-router-dom";
+import slide1 from "../../assets/picturesForProject/tents/camping/Indiana 4.jpg";
+import slide2 from "../../assets/picturesForProject/tents/camping/Indiana 4.jpg";
+import slide3 from "../../assets/picturesForProject/tents/camping/Indiana 4.jpg";
+import slide4 from "../../assets/picturesForProject/tents/camping/Indiana 4.jpg";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 
 const listPhoto = [slide1, slide2, slide3, slide4];
 
 const ProductPageSlider = () => {
+  const { listProductsAll } = useSelector((state) => state.productsReducers); // TODO: change reducer name
+  const { productId } = useParams();
+  const product = listProductsAll?.find(
+    (product) => product.product_id === +productId
+  );
+  console.log(product.product_img_path);
   const settings = {
     dots: true,
     infinite: true,
@@ -23,7 +31,7 @@ const ProductPageSlider = () => {
       <Slider {...settings}>
         {listPhoto.map((slide, index) => (
           <div key={index}>
-            <img src={slide} alt='true'/>
+            <img src={slide} alt="true" />
           </div>
         ))}
       </Slider>
