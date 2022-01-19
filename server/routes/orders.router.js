e8776ad6a6209dd299b4bf5b79626a7f56b0ca22
Sergio <dsbox@list.ru>
2022-01-19 18:32:16 +0300

    merge

--- server/routes/orders.router.js
+++ server/routes/orders.router.js
@@ -1,10 +1,10 @@
 const router = require('express').Router();
-const { Product, Order } = require('../db/models');
+const { Product, Order, OrderProduct } = require('../db/models');
 
 router
-  .route('/:id') // Все заказы на /products, здесь конкр юзер
+  .route('/:id') // Все заказы на /products, здесь конкр customer
   .get(async (req, res) => {
-    // const {} = req.body; // Theres no body in get req
+    // const {} = req.body; // TODO: if it would be GET, there's no body
     const { id } = req.params;
     try {
       const userOrders = await Order.findAll({
@@ -27,14 +27,20 @@ router
       userId,
       cart,
     } = req.body;
-    console.log(cart);
-
-    // const createOrder = await Order.create({
-    //   user_id: userId,
-
-    // })
 
     try {
+      const { id } = await Order.create({
+        user_id: userId,
+      });
+
+      cart.forEach(async (item) => {
+        await OrderProduct.create({
+          product_id: item.id,
+          order_id: id,
+          quantity: item.quantity,
+        });
+      });
+
       res.status(201).json({ message: 'Order created successfully' });
     } catch (err) {
       res.status(500).json({ login: false, message: err.message });
