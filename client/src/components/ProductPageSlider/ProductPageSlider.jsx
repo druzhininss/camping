73a5a2a37c414bdc9f4e29013f5ffe959aceb53b
SemyonSergeev <semyonsergeev30@gmail.com>
2022-01-19 11:49:30 +0300

    Добавил в seeder пути до файлов из assets, поменял пути в assets, а именно убрал пробелы и заменил их на подчеркивание, в компоненте ProductPageSlider изменил путь до картинки

--- client/src/components/ProductPageSlider/ProductPageSlider.jsx
+++ client/src/components/ProductPageSlider/ProductPageSlider.jsx
@@ -1,38 +1,40 @@
 import React from "react";
-import Slider from "react-slick";
+// import Slider from "react-slick";
 import { useSelector } from "react-redux";
 import { useParams } from "react-router-dom";
-// import slide1 from "../../assets/picturesForProject/tents/camping/Indiana 4.jpg";
-// import slide2 from "../../assets/picturesForProject/tents/camping/Indiana 4.jpg";
-// import slide3 from "../../assets/picturesForProject/tents/camping/Indiana 4.jpg";
-// import slide4 from "../../assets/picturesForProject/tents/camping/Indiana 4.jpg";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
+import rrr from '../../assets/picturesForProject/tents/camping/Indiana_4.jpg'
 
 const ProductPageSlider = () => {
+  // const pathToPicture = '../../assets/picturesForProject';
   const { listProductsAll } = useSelector((state) => state.productsReducers); // TODO: change reducer name
   const { productId } = useParams();
   const product = listProductsAll?.find(
     (product) => product.product_id === +productId
   );
-  const listPhoto = [product.product_img_path];
-  const settings = {
-    dots: true,
-    infinite: true,
-    speed: 500,
-    slidesToShow: 1,
-    slidesToScroll: 1,
-    arrows: true,
-  };
+  console.log(product);
+  // const listPhoto = [`${pathToPicture}${product.product_img_path}`];
+
+  // const settings = {
+  //   dots: true,
+  //   infinite: true,
+  //   speed: 500,
+  //   slidesToShow: 1,
+  //   slidesToScroll: 1,
+  //   arrows: true,
+  // };
+
   return (
     <div style={{ maxWidth: 700, height: 468, margin: "auto" }}>
-      <Slider {...settings}>
+      {/* <Slider {...settings}>
         {listPhoto.map((slide, index) => (
           <div key={index}>
             <img src={slide} alt="true" />
           </div>
         ))}
-      </Slider>
+      </Slider> */}
+      <img src={rrr} alt='true'/>
     </div>
   );
 };
