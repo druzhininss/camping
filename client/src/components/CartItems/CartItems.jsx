f8b221d643fc08edc7029f4715bc3ae8f856377d
Anya <makarobaanya@yandex.ru>
2022-01-19 19:40:05 +0300

    merge

--- client/src/components/CartItems/CartItems.jsx
+++ client/src/components/CartItems/CartItems.jsx
@@ -11,7 +11,7 @@ function CartItems({ cart }) {
   console.log(typeof cart.price)
   return (
     <div>
-      <div>
+      <div >
         <div className={style.cartCard}>
           <p className={style.cartP}>Название: {cart.name}</p>
           <p className={style.cartP}>Бренд: {cart.brand}</p>
@@ -19,11 +19,11 @@ function CartItems({ cart }) {
           <p className={style.cartP}>Цена товара: {cart.price}₽</p>
           <p className={style.cartP}>{cart.totalForItem}₽</p>
           <div className={style.btn}>
-            <button onClick={() => dispatch(decreaseProductsAC({ price: cart.price, id: cart.id }))}>-</button>
-            <p className={style.cartP}>{cart.quantity}</p>
-            <button onClick={() => dispatch(increaseProductsAC({ price: +cart.price, id: cart.id }))}>+</button>
+            <button className={style.cartB} onClick={() => dispatch(decreaseProductsAC({ price: cart.price, id: cart.id }))}>-</button>
+            <p className={style.cartB}>{cart.quantity}</p>
+            <button className={style.cartB} onClick={() => dispatch(increaseProductsAC({ price: +cart.price, id: cart.id }))}>+</button>
           </div>
-          <div>
+          <div className={style.cartP}>
             <button className={style.button} onClick={() => dispatch(deleteProductsAC({ id: cart.id, price: cart.totalForItem }))}>Удалить товар</button>
           </div>
         </div>
