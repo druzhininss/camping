26e671ff7dab9ce71b6ee8fa8e738f657ca5b30e
Denis <bokudenis065@gmail.com>
2022-01-20 20:23:55 +0300

    profile prefinal version

--- client/src/components/UserProfileOrders/UserProfileOrders.jsx
+++ client/src/components/UserProfileOrders/UserProfileOrders.jsx
@@ -1,24 +1,25 @@
 /* eslint-disable react/prop-types */
 import React from 'react';
+import style from './UserProfile.module.css';
 
 function UserProfileOrders({ order }) {
   const pathToPicture = "/img/picturesForProject";
 
   return (
     <div>
-      <div>
+      <div className={style.profileOrders}>
         <div>
-          <p>{order.productName}</p>
+          <p>Наименование товара:{order.productName}</p>
         </div>
         <div >
           <img style={{ maxWidth: 200, height: 200, margin: "auto" }} src={`${pathToPicture}${order.imagePath}`}></img>
         </div>
         <ul>
           <li>
-            <span>{order.quantity}</span>
+            <span>Кол-во: {order.quantity}</span>
           </li>
           <li>
-            <span>{order.date}</span>
+            <span>Дата-заказа: {order.date}</span>
           </li>
         </ul>
       </div>
