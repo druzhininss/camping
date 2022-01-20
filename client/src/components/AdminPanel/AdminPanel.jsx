5bfdc50916f850b0496bd9f046804d604f326b2c
SemyonSergeev <semyonsergeev30@gmail.com>
2022-01-20 22:54:09 +0300

    Отцентрировал див с кнопками в панели админа

--- client/src/components/AdminPanel/AdminPanel.jsx
+++ client/src/components/AdminPanel/AdminPanel.jsx
@@ -1,29 +1,53 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import { useDispatch, useSelector } from 'react-redux';
-import AdminPanelLogin from '../AdminPanelLogin/AdminPanelLogin.jsx';
-import { adminLogoutAC } from '../../redux/actionCreators/adminLogoutAC';
-import style from './AdminPanel.module.css';
+import React from "react";
+import { Link } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import AdminPanelLogin from "../AdminPanelLogin/AdminPanelLogin.jsx";
+import { adminLogoutAC } from "../../redux/actionCreators/adminLogoutAC";
+import style from "./AdminPanel.module.css";
 
 function AdminPanel() {
   const { isAdmin } = useSelector((state) => state.productsReducers);
   const dispatch = useDispatch();
 
   return (
-    <>
-      {!isAdmin
-        ?
+    <div className={style.container}>
+      {!isAdmin ? (
         <div>
-          <AdminPanelLogin />  {/*когда будет готов isAdmin то этот див с компонетом поменять местами с кнопками */}
+          <AdminPanelLogin />{" "}
+          {/*когда будет готов isAdmin то этот див с компонетом поменять местами с кнопками */}
         </div>
-        :
-        <div className='order_flex'>
-          <button className={style.order} ><Link to='/admin/orders'>Заказы</Link></button>
-          <button className={style.order} ><Link to='/admin/products'>Товары</Link></button>
-          <button><Link to='/admin/' onClick={() => dispatch(adminLogoutAC())}>Выход из админ панели</Link></button>
+      ) : (
+        <div>
+          <div className="d-flex justify-content-around mb-5">
+            <button className="btn btn-success btn-lg">
+              <Link
+                style={{ textDecoration: "none", color: "white" }}
+                to="/admin/orders"
+              >
+                Заказы
+              </Link>
+            </button>
+            <button className="btn btn-success btn-lg">
+              <Link
+                style={{ textDecoration: "none", color: "white" }}
+                to="/admin/products"
+              >
+                Товары
+              </Link>
+            </button>
+          </div>
+          <button className="btn btn-danger btn-sm">
+            <Link
+              style={{ textDecoration: "none", color: "white" }}
+              to="/admin/"
+              onClick={() => dispatch(adminLogoutAC())}
+            >
+              Выход
+            </Link>
+          </button>
         </div>
-      }
-    </>
+      )}
+    </div>
   );
 }
 
