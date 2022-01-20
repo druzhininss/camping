2ce213db21ba1563f346406506486694f45a071a
SemyonSergeev <semyonsergeev30@gmail.com>
2022-01-20 15:16:32 +0300

    Первоначальный вариант вертски администраторской панели

--- client/src/components/AdminPanelProductItems/AdminPanelProductItems.jsx
+++ client/src/components/AdminPanelProductItems/AdminPanelProductItems.jsx
@@ -4,6 +4,8 @@ import { changeItemsProductAC } from '../../redux/actionCreators/adminAC';
 import { deleteItemsProductAC } from '../../redux/actionCreators/adminAC';
 import { saveChangeItemsProductAC } from '../../redux/actionCreators/adminAC';
 
+const classes = ['mt-3 pe-3 ps-3 d-flex flex-column border border-5 rounded-2 bg-light shadow']
+
 function AdminPanelProductItems(product) {
   const dispatch = useDispatch();
   const { change } = product.product
@@ -21,32 +23,32 @@ function AdminPanelProductItems(product) {
     }
   }
   return (
-    <div>
+    <div className={classes}>
       <div>
         <p>{product.product.product_name}</p>
       </div>
       <div >
-        <img style={{ maxWidth: 200, height: 200, margin: "auto" }} ></img>
+        <img></img>
       </div>
       <ul>
         <li>
           <img src=""></img>
-          {change ? <input type="text" ref={weight} placeholder="Вес"></input> : <span>{product.product.Вес}</span>}
+          {change ? <input type="text" ref={weight} placeholder="Вес"></input> : <span>Вес: {product.product.Вес}</span>}
         </li>
         <li>
           <img src=""></img>
-          {change ? <input type="text" ref={use} placeholder="Область применения"></input> : <span>{product.product['Область применения']}</span>}
+          {change ? <input type="text" ref={use} placeholder="Область применения"></input> : <span>Категория: {product.product['Область применения']}</span>}
         </li>
         <li>
           <img src=""></img>
-          {change ? <input type="text" ref={price} placeholder="Цена"></input> : <span>{product.product.Цена}</span>}
+          {change ? <input type="text" ref={price} placeholder="Цена"></input> : <span>Стоимость: {product.product.Цена}</span>}
         </li>
       </ul>
       {change ?
         <button onClick={() => dispatch(saveChangeItemsProductAC(changeItemsProduct()))}>Сохранить</button> :
         <div>
-          <button onClick={() => dispatch(changeItemsProductAC(product.product.product_id))}>Изменить</button>
-          <button onClick={() => dispatch(deleteItemsProductAC(product.product.product_id))}>Удалить</button>
+          <button className='btn btn-success' onClick={() => dispatch(changeItemsProductAC(product.product.product_id))}>Изменить</button>
+          <button className='btn btn-warning ms-1' onClick={() => dispatch(deleteItemsProductAC(product.product.product_id))}>Удалить</button>
         </div>
       }
     </div>
