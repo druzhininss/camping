5d1d14630a87dd78eee7c17d60f9259cea82e6e6
Sergio <dsbox@list.ru>
2022-01-20 16:32:01 +0300

    fix

--- client/src/components/AdminPanel/AdminPanel.jsx
+++ client/src/components/AdminPanel/AdminPanel.jsx
@@ -5,17 +5,17 @@ import AdminPanelLogin from '../AdminPanelLogin/AdminPanelLogin.jsx';
 import style from './AdminPanel.module.css';
 
 function AdminPanel() {
-  const { chechAdmin } = useSelector((state) => state.productsReducers);
+  const { checkAdmin } = useSelector((state) => state.productsReducers);
   
   return (
     <>
-      {chechAdmin ?
+      {checkAdmin ?
         <div>
           <AdminPanelLogin />  {/*когда будет готов isAdmin то этот див с компонетом поменять местами с кнопками */}
         </div> :
         <div className='order_flex'>
           <button className={style.order} ><Link to='/admin/order'>Заказы</Link></button>
-          <button className={style.order} ><Link c to='/admin/products'>Товары</Link></button>
+          <button className={style.order} ><Link to='/admin/products'>Товары</Link></button>
         </div>
       }
     </>
