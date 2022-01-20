9a47993fe86876b5c4c892c1cab100f102f173dd
Vladimir <vladimirdeveloper99@gmail.com>
2022-01-20 22:07:08 +0300

    layout feedback

--- client/src/components/Feedback/Feedback.jsx
+++ client/src/components/Feedback/Feedback.jsx
@@ -5,97 +5,97 @@ function Feedback() {
   return (
     <div>
       <h1>Отзывы</h1>
-    <div className={style.feedback}>
-      <div className={style.koguvcavis_varazdel}>
-        <div className={style.sestim_donials}>
-          <div className={style.sectionesag}></div>
-          <div className={style.sagestim_lonials}>
-            <div className={style.vemotau_vokusipol}>
-              <div className={style.testimonial}>
-                <img src="http://zornet.ru/_fr/93/3806974.jpg" alt=""></img>
-                <div className={style.gecedanam}>Лена</div>
-                <div className={style.apogered_gselected}>
-                  <i className={style.fas} ></i>
-                  <i className={style.fas}></i>
-                  <i className="fas fa-star"></i>
-                  <i className="fas fa-star"></i>
-                  <i className="fas fa-star"></i>
-                </div>
+      <div className={style.feedback}>
+        <div className={style.koguvcavis_varazdel}>
+          <div className={style.sestim_donials}>
+            <div className={style.sectionesag}></div>
+            <div className={style.sagestim_lonials}>
+              <div className={style.vemotau_vokusipol}>
+                <div className={style.testimonial}>
+                  <img src="http://zornet.ru/_fr/93/3806974.jpg" alt=""></img>
+                  <div className={style.gecedanam}>Лена</div>
+                  <div className={style.apogered_gselected}>
+                    <i className={style.fas} ></i>
+                    <i className={style.fas}></i>
+                    <i className="fas fa-star"></i>
+                    <i className="fas fa-star"></i>
+                    <i className="fas fa-star"></i>
+                  </div>
 
-                <p>Здесь первое описание. Продолжение.</p>
+                  <p>Здесь первое описание. Продолжение</p>
+                </div>
               </div>
             </div>
           </div>
         </div>
-      </div>
-      <div className={style.koguvcavis_varazdel}>
-        <div className={style.sestim_donials}>
-          <div className={style.sectionesag}></div>
-          <div className={style.sagestim_lonials}>
-            <div className={style.vemotau_vokusipol}>
-              <div className={style.testimonial}>
-                <img src="http://zornet.ru/_fr/83/7890600.jpg" alt=""></img>
-                <div className={style.gecedanam}>Oleg</div>
-                <div className={style.apogered_gselected}>
-                  <i className="fas fa-star"></i>
-                  <i className="fas fa-star"></i>
-                  <i className="fas fa-star"></i>
-                  <i className="fas fa-star"></i>
-                  <i className="fas fa-star"></i>
-                </div>
+        <div className={style.koguvcavis_varazdel}>
+          <div className={style.sestim_donials}>
+            <div className={style.sectionesag}></div>
+            <div className={style.sagestim_lonials}>
+              <div className={style.vemotau_vokusipol}>
+                <div className={style.testimonial}>
+                  <img src="http://zornet.ru/_fr/83/7890600.jpg" alt=""></img>
+                  <div className={style.gecedanam}>Oleg</div>
+                  <div className={style.apogered_gselected}>
+                    <i className="fas fa-star"></i>
+                    <i className="fas fa-star"></i>
+                    <i className="fas fa-star"></i>
+                    <i className="fas fa-star"></i>
+                    <i className="fas fa-star"></i>
+                  </div>
 
-                <p>Почему у всех одинаковые фото.</p>
+                  <p>Почему у всех одинаковые фото.</p>
+                </div>
               </div>
             </div>
           </div>
         </div>
-      </div>
-      <div className={style.koguvcavis_varazdel}>
-        <div className={style.sestim_donials}>
-          <div className={style.sectionesag}></div>
-          <div className={style.sagestim_lonials}>
-            <div className={style.vemotau_vokusipol}>
-              <div className={style.testimonial}>
-                <img src="http://zornet.ru/_fr/93/7140380.jpg" alt=""></img>
-                <div className={style.gecedanam}>Юра</div>
-                <div className={style.apogered_gselected}>
-                  <i className={style.fas}></i>
-                  <i className={style.fas}></i>
-                  <i className={style.fas}></i>
-                  <i className={style.fas}></i>
-                  <i className="fas fa-star"></i>
-                </div>
+        <div className={style.koguvcavis_varazdel}>
+          <div className={style.sestim_donials}>
+            <div className={style.sectionesag}></div>
+            <div className={style.sagestim_lonials}>
+              <div className={style.vemotau_vokusipol}>
+                <div className={style.testimonial}>
+                  <img src="http://zornet.ru/_fr/93/7140380.jpg" alt=""></img>
+                  <div className={style.gecedanam}>Юра</div>
+                  <div className={style.apogered_gselected}>
+                    <i className={style.fas}></i>
+                    <i className={style.fas}></i>
+                    <i className={style.fas}></i>
+                    <i className={style.fas}></i>
+                    <i className="fas fa-star"></i>
+                  </div>
 
-                <p>Здесь первое описание. Продолжение.</p>
+                  <p>Здесь первое описание. Продолжение.</p>
+                </div>
               </div>
             </div>
           </div>
         </div>
-      </div>
-      <div className={style.koguvcavis_varazdel}>
-        <div className={style.sestim_donials}>
-          <div className={style.sectionesag}></div>
-          <div className={style.sagestim_lonials}>
-            <div className={style.vemotau_vokusipol}>
-              <div className={style.testimonial}>
-                <img src="http://zornet.ru/_fr/93/4298253.jpg" alt=""></img>
-                <div className={style.gecedanam}>Аня</div>
-                <div className={style.apogered_gselected}>
-                  <i className={style.fas}></i>
-                  <i className="fas fa-star"></i>
-                  <i className="fas fa-star"></i>
-                  <i className="fas fa-star"></i>
-                  <i className="fas fa-star"></i>
-                </div>
+        <div className={style.koguvcavis_varazdel}>
+          <div className={style.sestim_donials}>
+            <div className={style.sectionesag}></div>
+            <div className={style.sagestim_lonials}>
+              <div className={style.vemotau_vokusipol}>
+                <div className={style.testimonial}>
+                  <img src="http://zornet.ru/_fr/93/4298253.jpg" alt=""></img>
+                  <div className={style.gecedanam}>Аня</div>
+                  <div className={style.apogered_gselected}>
+                    <i className={style.fas}></i>
+                    <i className="fas fa-star"></i>
+                    <i className="fas fa-star"></i>
+                    <i className="fas fa-star"></i>
+                    <i className="fas fa-star"></i>
+                  </div>
 
-                <p>Звоню вам на лифчик, не кто не отвечает!</p>
+                  <p>Звоню вам на лифчик, не кто не отвечает!</p>
+                </div>
               </div>
             </div>
           </div>
         </div>
       </div>
     </div>
-    </div> 
   );
 }
 
